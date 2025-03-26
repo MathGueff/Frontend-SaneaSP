f4d7702b07592b04bf09c1ee7c8e36b8a7ffde25
Ryan <ryanjobs234@gmail.com>
2025-03-26 19:28:59 -0300

    Criação do Delete Notícia

---
+++ PrjInterdisciplinar/src/app/Noticias/dell-noticia/dell-noticia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dell-noticia',
+  standalone: true,
+  imports: [],
+  templateUrl: './dell-noticia.component.html',
+  styleUrl: './dell-noticia.component.css'
+})
+export class DellNoticiaComponent {
+
+}
