797f63dabcb5dbf60950200d1be6f3c3564456b1
devDavyRibeiro <you@example.comdavy.oliv.ribeiro@gmail.com>
2025-03-26 12:07:22 -0300

    impedindo o usuário não logado de acessar a parte de suas-reclamações

--- PrjInterdisciplinar/src/app/Reclamacao/reclamacao-usuarios/reclamacao-usuarios.component.ts
+++ PrjInterdisciplinar/src/app/Reclamacao/reclamacao-usuarios/reclamacao-usuarios.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ReclamacaoCardComponent } from '../reclamacao-card/reclamacao-card.component';
 import { NotFoundComponent } from '../../Common/not-found/not-found.component';
 import { Reclamacao } from '../../models/reclamacao';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { UserService } from '../../Services/user.service';
+import { IUser } from '../../models/user.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-reclamacao-usuarios',
@@ -12,9 +16,11 @@ import { BehaviorSubject, Observable } from 'rxjs';
   styleUrl: './reclamacao-usuarios.component.css'
 })
 export class ReclamacaoUsuariosComponent implements OnInit {
+  private userService = inject(UserService);
+  private router = inject(Router);
   private reclamacaoSubject =new BehaviorSubject<Reclamacao[]>([] as any);
   data$:Observable<Reclamacao[]> = this.reclamacaoSubject.asObservable();
-
+  protected usuario : IUser | null = this.userService.getCurrentUser();
   erro : string = "";
   reclamacoes: Reclamacao[] = [
       {
@@ -31,9 +37,25 @@ export class ReclamacaoUsuariosComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.usuario = {
+      id: 2,
+      nome: 'Davy',
+      email: 'davy@gmail.com',
+      senha: 'davy',
+      endereco:{
+        cep: '17571802',
+        bairro : 'Jardim Europa',
+        logradouro : 'Rua Rock',
+        cidade : 'Votorantim'
+      }}
+
+    if(!this.usuario){
+      this.router.navigate(['']);
+    }
+
     // let lista : Reclamacao [];
     // lista = this.reclamacoes.find((reclamacao) =>{
-    //   if()
+    //   if(reclamacao.)
     // })
   }
 }
