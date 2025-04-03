e2c50baaa3e569902ade295935f6ebbd1c6f8f37
Ryan <ryanjobs234@gmail.com>
2025-04-02 22:10:30 -0300

    Falta tudo do deletar noticia

--- PrjInterdisciplinar/src/app/Noticias/dell-noticia/dell-noticia.component.ts
+++ PrjInterdisciplinar/src/app/Noticias/dell-noticia/dell-noticia.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dell-noticia',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule],
   templateUrl: './dell-noticia.component.html',
   styleUrl: './dell-noticia.component.css'
 })
-export class DellNoticiaComponent {
 
-  pesquisarNoticia() {
+export class DellNoticiaComponent {
+  @ViewChild('idNoticia') idNoticia: any;
+
+  idEncontrado: boolean = false;
+  idPesquisado: number = 0;
+  listaNoticias: number[] = [1,2,3,4,5];
+
+  pesquisarId() {
+    this.idPesquisado = Number(this.idNoticia.nativeElement.value);
+    this.idEncontrado = this.listaNoticias.includes(this.idPesquisado);
+  }
+
+  openModal() {
     
   }
 }
