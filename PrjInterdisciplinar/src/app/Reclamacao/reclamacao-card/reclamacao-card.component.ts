3a30f5bebd97389b942a9fe07909c4d74aa3e11d
devDavyRibeiro <you@example.comdavy.oliv.ribeiro@gmail.com>
2025-04-05 17:40:38 -0300

    colocando acessibilidade

--- PrjInterdisciplinar/src/app/Reclamacao/reclamacao-card/reclamacao-card.component.ts
+++ PrjInterdisciplinar/src/app/Reclamacao/reclamacao-card/reclamacao-card.component.ts
@@ -1,5 +1,5 @@
 import { UserService } from './../../Services/user.service';
-import { Component, inject, Input} from '@angular/core';
+import { Component, ElementRef, inject, Input, ViewChild} from '@angular/core';
 import { Reclamacao } from '../../models/class/reclamacao';
 import { RouterLink } from '@angular/router';
 import { SweetAlertService } from '../../Services/sweetAlert.service';
@@ -14,9 +14,12 @@ import { CommonModule } from '@angular/common';
 })
 export class ReclamacaoCardComponent   {
   @Input () card !: Reclamacao;
+  @ViewChild('dropdownLinks') linksDropdown !: ElementRef;
+  @ViewChild('dropdown') dropdown !: ElementRef;
   private sweetAlert = inject(SweetAlertService);
   private userService = inject(UserService);
   user$ = this.userService.getObservableCurrentUser();
+
   // Função para excluir Reclamacao. Função assincrona: precisa usar async e await
   protected async exclusaoReclamacao(id:number){
     const confirm = await this.sweetAlert.confirmExclusion(`Deseja mesmo exluir a Reclamação: ${id}?`);
@@ -24,6 +27,14 @@ export class ReclamacaoCardComponent   {
     if(confirm){
       this.sweetAlert.showMessage("Reclamação Excluída com sucesso");
     }
+  }
+
+  protected toggleOptionsDropdown() {
+    const dropdown = this.linksDropdown.nativeElement as HTMLElement;
+    if(dropdown.classList.contains("show"))
+      dropdown.classList.remove('show');
+    else
+      dropdown.classList.add('show')
   }
 }
 
