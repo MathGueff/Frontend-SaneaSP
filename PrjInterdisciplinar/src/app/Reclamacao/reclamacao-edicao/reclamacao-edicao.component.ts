4b8589a09e6c80a6e6dfc7a43dfd29c501335bd8
Davy Ribeiro <davy.oliv.ribeiro@gmail.com>
2025-05-20 20:09:06 -0300

    fix: corrigindo os nomes das variaveis para não dar erro

--- PrjInterdisciplinar/src/app/Reclamacao/reclamacao-edicao/reclamacao-edicao.component.ts
+++ PrjInterdisciplinar/src/app/Reclamacao/reclamacao-edicao/reclamacao-edicao.component.ts
@@ -7,6 +7,7 @@ import { Reclamacao } from '../../models/class/reclamacao';
 import { UserService } from '../../Services/user.service';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { NotFoundComponent } from '../../Common/not-found/not-found.component';
+import { IReclamacao } from '../../models/interface/IReclamacao.interface';
 
 @Component({
   selector: 'app-reclamacao-edicao',
@@ -25,127 +26,42 @@ export class ReclamacaoEdicaoComponent implements OnInit {
     protected vazio : boolean = false;
     protected path :string = "../";
 
-    private reclamacao ?: Reclamacao
-    reclamacoes: Reclamacao [] = [
+    private reclamacao ?: IReclamacao
+    reclamacoes: IReclamacao [] = [
       {
-        idReclamacao: 1,
-        tituloReclamao: "Falta de abastecimento de água",
-        descricaoReclamacao: "Há três dias o bairro está sem água, afetando diversas famílias. A situação está insustentável, pois as pessoas não conseguem realizar atividades básicas como cozinhar, tomar banho ou lavar roupas. Entramos em contato com a companhia de saneamento, mas até agora não houve retorno sobre o motivo da interrupção ou previsão de normalização.",
-        dataReclamacao: "2024-11-28",
-        objTag: "Abastecimento",
-        objImagem : "img/paginas/reclamacoes/user1.jpg",
-        objUsuario :{
-          id: 1,
-          nome: 'Matheus',
-          email: 'gueff@gmail.com',
-          senha: 'math',
-          endereco:{
-            cep: '18075718',
-            bairro : 'Jardim Brasilândia',
-            logradouro : 'Rua Alonco Muchon',
-            cidade : 'Sorocaba'
-          }
-        }
+        id: 1,
+        titulo: "Falta de abastecimento de água",
+        descricao: "Há três dias o bairro está sem água, afetando diversas famílias. A situação está insustentável, pois as pessoas não conseguem realizar atividades básicas como cozinhar, tomar banho ou lavar roupas. Entramos em contato com a companhia de saneamento, mas até agora não houve retorno sobre o motivo da interrupção ou previsão de normalização.",
+        data: new Date ("2024-11-28"),
+        Tag: ["Abastecimento"],
+        Imagem : ["img/paginas/reclamacoes/user1.jpg"],
+        cep: '18075718',
+        bairro : 'Jardim Brasilândia',
+        rua : 'Rua Alonco Muchon',
+        cidade : 'Sorocaba',
+        pontuacao:200,
+        status: 1
       },
-      {
-        idReclamacao: 2,
-        tituloReclamao: "Vazamento de esgoto na rua",
-        descricaoReclamacao: "Esgoto está vazando na rua há mais de uma semana e ninguém resolve. O odor é insuportável, e a situação está causando transtornos aos moradores e comerciantes da região. Além disso, o esgoto acumulado atrai insetos e representa um grave risco à saúde pública, especialmente para crianças que brincam no local.",
-        dataReclamacao: "2024-11-27",
-        objTag: "Esgoto",
-        objImagem : "img/paginas/reclamacoes/user2.jpg",
-        objUsuario:{
-          id: 2,
-          nome: 'Davy',
-          email: 'davy@gmail.com',
-          senha: 'davy',
-          endereco:{
-            cep: '17571802',
-            bairro : 'Jardim Europa',
-            logradouro : 'Rua Rock',
-            cidade : 'Votorantim'
-          }
-        }
-      },
-      {
-        idReclamacao: 3,
-        tituloReclamao: "Falta de coleta de lixo",
-        descricaoReclamacao: "A coleta de lixo não foi realizada no bairro nos últimos cinco dias, acumulando sacos de lixo nas ruas. Isso tem atraído animais como ratos e baratas, gerando um risco à saúde pública. Diversos moradores já entraram em contato com a prefeitura, mas ainda não há previsão para regularizar o serviço.",
-        dataReclamacao: "2024-11-26",
-        objTag: "Lixo",
-        objImagem : "img/paginas/reclamacoes/user3.jpg",
-        objUsuario:{
-          id: 3,
-          nome: 'Adryann',
-          email: 'adryann@gmail.com',
-          senha: 'adry',
-          endereco:{
-            cep: '11111111',
-            bairro : 'Bairro tal',
-            logradouro : 'Rua tal',
-            cidade : 'Sorocaba'
-          }
-        }
-      },
-      {
-        idReclamacao: 4,
-        tituloReclamao: "Água contaminada",
-        descricaoReclamacao: "A água fornecida pelo sistema público está com um odor forte e coloração amarelada. Diversos moradores já relataram problemas gastrointestinais após o consumo, mesmo depois de ferver a água. Não houve qualquer comunicado oficial sobre a causa do problema, e isso está gerando preocupação generalizada no bairro.",
-        dataReclamacao: "2024-11-25",
-        objTag: "Qualidade da Água",
-        objImagem : "img/paginas/reclamacoes/user4.jpg",
-        objUsuario:{
-          id: 2,
-          nome: 'Davy',
-          email: 'davy@gmail.com',
-          senha: 'davy',
-          endereco:{
-            cep: '17571802',
-            bairro : 'Jardim Europa',
-            logradouro : 'Rua Rock',
-            cidade : 'Votorantim'
-          }
-        }
-      },
-      {
-        idReclamacao: 5,
-        tituloReclamao: "Erosão em área de drenagem",
-        descricaoReclamacao: "A má manutenção do sistema de drenagem causou erosões no terreno, colocando em risco casas próximas. A água da chuva não está sendo drenada adequadamente, resultando em enchentes frequentes e agravando os danos. Os moradores estão preocupados com a possibilidade de deslizamentos.",
-        dataReclamacao: "2024-11-24",
-        objTag: "Drenagem",
-        objImagem : "img/paginas/reclamacoes/user5.jpg",
-        objUsuario: {
-          id: 4,
-          nome: 'Ryan',
-          email: 'ryan@gmail.com',
-          senha: 'ryan',
-          endereco:{
-            cep: '11111111',
-            bairro : 'Bairro tal',
-            logradouro : 'Rua tal',
-            cidade : 'Sorocaba'
-          }
-        }
-      },
+
     ];
     rows: number = 2;
     src: any = null;
 
 
     form = this.formBuider.group({
-      titulo: [this.reclamacao?.tituloReclamao, [Validators.required]],
-      descricao: [this.reclamacao?.descricaoReclamacao, [Validators.required]],
+      titulo: [this.reclamacao?.titulo, [Validators.required]],
+      descricao: [this.reclamacao?.descricao, [Validators.required]],
       cep: [
-        this.reclamacao?.objEndereco?.cep,
+        this.reclamacao?.cep,
         [Validators.required, Validators.minLength(8), Validators.maxLength(8)],
       ],
-      numero: [this.reclamacao?.objEndereco?.numero, [Validators.required]],
-      cidade: [this.reclamacao?.objEndereco?.cidade, [Validators.required]],
-      bairro: [this.reclamacao?.objEndereco?.bairro, [Validators.required]],
-      rua: [this.reclamacao?.objEndereco?.logradouro, [Validators.required]],
-      complemento: [this.reclamacao?.objEndereco?.complemento],
-      tag: [this.reclamacao?.objTag],
-      imagem: [this.reclamacao?.objImagem],
+      numero: [this.reclamacao?.numero, [Validators.required]],
+      cidade: [this.reclamacao?.cidade, [Validators.required]],
+      bairro: [this.reclamacao?.bairro, [Validators.required]],
+      rua: [this.reclamacao?.rua, [Validators.required]],
+      complemento: [this.reclamacao?.complemento],
+      Tag: [this.reclamacao?.Tag],
+      Imagem: [this.reclamacao?.Imagem],
     });
 
     onSubmit() {
@@ -157,7 +73,7 @@ export class ReclamacaoEdicaoComponent implements OnInit {
     ngOnInit(): void {
       this.activeRouter.params.subscribe((params)=>{
         this.reclamacao = this.reclamacoes.find((objReclamacao)=>{
-          return objReclamacao.idReclamacao ===  Number(params['id']);
+          return objReclamacao.id ===  Number(params['id']);
         })
         if(!this.reclamacao){
           this.vazio = true;
@@ -165,15 +81,13 @@ export class ReclamacaoEdicaoComponent implements OnInit {
           return;
         }
         this.form.patchValue({
-          titulo: this.reclamacao.tituloReclamao,
-          descricao: this.reclamacao?.descricaoReclamacao,
-          cep: this.reclamacao.objEndereco?.cep,
-          cidade: this.reclamacao.objEndereco?.cidade,
-          rua: this.reclamacao.objEndereco?.logradouro,
-          bairro: this.reclamacao.objEndereco?.bairro,
-          complemento: this.reclamacao.objEndereco?.complemento,
-          tag: this.reclamacao.objTag,
-          imagem: this.reclamacao.objImagem
+          titulo: this.reclamacao.titulo,
+          descricao: this.reclamacao?.descricao,
+          cep: this.reclamacao.cep,
+          cidade: this.reclamacao.cidade,
+          rua: this.reclamacao.rua,
+          bairro: this.reclamacao.bairro,
+          complemento: this.reclamacao.complemento,
         });
         console.log(this.reclamacao);
       })
@@ -200,7 +114,7 @@ export class ReclamacaoEdicaoComponent implements OnInit {
           if (response.bairro) {
             this.setAddressControl('bairro', response.bairro);
           } else {
-            console.log('O logradouro não foi encontrado para o CEP informado.');
+            console.log('O rua não foi encontrado para o CEP informado.');
           }
 
           if (response.localidade) {
