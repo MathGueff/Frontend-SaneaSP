929c1fa980eca449da9ea18545b4ad48c2d3c7a2
Davy Ribeiro <davy.oliv.ribeiro@gmail.com>
2025-06-03 18:29:15 -0300

    feat: adicionando mais comportamento ao tag-selected

--- PrjInterdisciplinar/src/app/Common/tag-select/tag-select.component.ts
+++ PrjInterdisciplinar/src/app/Common/tag-select/tag-select.component.ts
@@ -15,32 +15,34 @@ import { TagService } from '../../Services/tag.service';
 export class TagSelectComponent {
   private tagService = inject(TagService);
   public formTags !: ITag[];
-  public select = new FormControl<ITag | null>(null)
-  public formSelects : ITag[] = []
+  protected tagNull : ITag = {
+    id: 0,
+    nome: "Nenhum"
+  }
+  public select = new FormControl<ITag | null>(this.tagNull);
+  public tagSelected : ITag[] = [];
   constructor() {
     this.tagService.getTagsList().subscribe({
       next: (tags) => this.formTags = tags.data
     })
+
     this.select.valueChanges.subscribe((tag)=>{
       if(tag){
         if(tag.id !== 0){
-          if(! this.formSelects.find((tagArray) =>{
-            return tagArray.id === tag.id
-          })){
-            this.formSelects.push(tag);
-            console.log(this.formSelects)
+          const existsTag = this.tagSelected.find((tagArray) => tagArray.id === tag.id)
+          if(!existsTag){
+            this.tagSelected.push(tag);
+            console.log(this.tagSelected)
           }
         }
-
-
       }
     })
   }
 
- protected TirarTagSelect( tag: ITag){
-  let index = this.formSelects.indexOf(tag)
+ protected RemoveTagSelect( tag: ITag){
+  let index = this.tagSelected.indexOf(tag)
   if(index !== -1){
-    this.formSelects.splice(index,1)
+    this.tagSelected.splice(index,1)
   }
  }
 
