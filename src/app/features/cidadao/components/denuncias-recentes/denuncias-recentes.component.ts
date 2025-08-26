89884c0c73eeac576a4f000842edf86792e24b67
MathGueff <gueffmatheus@gmail.com>
2025-08-25 21:15:09 -0300

    feat(area-cidadao): adiciona section de denuncias recentes

--- src/app/features/cidadao/components/denuncias-recentes/denuncias-recentes.component.ts
+++ src/app/features/cidadao/components/denuncias-recentes/denuncias-recentes.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-denuncias-recentes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './denuncias-recentes.component.html',
   styleUrl: './denuncias-recentes.component.css'
 })
 export class DenunciasRecentesComponent {
-
+  teste : {description : string}[] = [
+    {description : "ver since the 1 remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh"},
+    {description : "ver since the 1 remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh"},
+    {description : "ver since the 1 remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh"},
+    {description : "ver since the 1 remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh"},
+    {description : "ver since the 1 remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh"}
+  ]
 }
