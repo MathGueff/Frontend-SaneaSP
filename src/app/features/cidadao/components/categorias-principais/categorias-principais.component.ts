22cdf184cbd7c291d372b627f2f2dfe5c91c5288
MathGueff <gueffmatheus@gmail.com>
2025-09-01 21:35:37 -0300

    feat(area_cidadao): adiciona section de categorias principais

--- src/app/features/cidadao/components/categorias-principais/categorias-principais.component.ts
+++ src/app/features/cidadao/components/categorias-principais/categorias-principais.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-categorias-principais',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categorias-principais.component.html',
-  styleUrl: './categorias-principais.component.css'
+  styleUrls: ['./categorias-principais.component.css', '../../pages/menu-cidadao/menu-cidadao.component.css']
 })
 export class CategoriasPrincipaisComponent {
-
+  categories = [
+    {name: 'Poluição'},
+    {name: 'Saneamento básico'},
+    {name: 'Poluição'},
+    {name: 'Saneamento básico'},
+    {name: 'Poluição'},
+    {name: 'Saneamento básico'},
+    {name: 'Poluição'},
+    {name: 'Saneamento básico'},
+    {name: 'Saneamento básico'}
+  ]
 }
