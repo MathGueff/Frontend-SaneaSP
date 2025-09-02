a87513fab65b7ffd4e58e46bb14a638e80df6e13
MathGueff <gueffmatheus@gmail.com>
2025-09-01 22:07:51 -0300

    feat(area_cidadao): adiciona section de denuncias gerais

--- src/app/features/cidadao/components/denuncias-gerais/denuncias-gerais.component.ts
+++ src/app/features/cidadao/components/denuncias-gerais/denuncias-gerais.component.ts
@@ -5,8 +5,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './denuncias-gerais.component.html',
-  styleUrl: './denuncias-gerais.component.css'
+  styleUrls: [
+    './denuncias-gerais.component.css',
+    '../../pages/menu-cidadao/menu-cidadao.component.css',
+    '../../styles/denuncia.css'
+  ]
 })
 export class DenunciasGeraisComponent {
-
+  teste: { description: string }[] = [
+    { description: "Na minha rua o esgoto corre a céu aberto há meses, o cheiro é insuportável e ninguém da prefeitura aparece para resolver." },
+    { description: "Moro perto de um córrego que está cheio de lixo e esgoto, as crianças brincam por ali e vivem ficando doentes." },
+    { description: "Aqui no bairro não temos coleta de lixo regular, o caminhão passa quando quer e o lixo fica acumulado na calçada atraindo ratos." },
+    { description: "Toda vez que chove a água volta pelo ralo do banheiro porque a rede de esgoto está entupida. Já fizemos várias reclamações." }
+  ];
 }
