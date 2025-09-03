5cfca582455975930b804a6ff67d1d3efbc8cb3e
MathGueff <gueffmatheus@gmail.com>
2025-09-02 21:57:53 -0300

    feat(area_cidadao): adiciona links principais ao nav do header do cidadao

--- src/app/core/components/header/header-cidadao/header-cidadao.component.ts
+++ src/app/core/components/header/header-cidadao/header-cidadao.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header-cidadao',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header-cidadao.component.html',
   styleUrls: ['./header-cidadao.component.css','../header.component.css']
 })
 export class HeaderCidadaoComponent {
   navbarLinks : {path : string, name : string}[] = [
+    {path: '#banner', name: 'Faça sua denúncia'},
     {path: '#banner', name: 'Denuncias'},
-    {path: '#main', name: 'Organizacao'},
+    {path: '#main', name: 'Entrar como organização'},
   ]
 }
