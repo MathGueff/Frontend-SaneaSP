f366727232ffa23a57100393b49e6de349570f4c
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-09-09 10:36:26 -0300

    fix: corrige .spec de app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'PrjIntegrador' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('PrjIntegrador');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
