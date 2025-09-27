2bc2e7be7a81ec8cdde2ee5074e208e45850e08f
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-09-27 18:42:22 -0300

    refactor(cadastro_denuncia): ajusta CSS de steps para melhor identificação de clique

--- src/app/features/denuncia/cadastro/components/form-steps/form-steps.component.ts
+++ src/app/features/denuncia/cadastro/components/form-steps/form-steps.component.ts
@@ -1,36 +1,39 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ISteps, StepsTypes } from '../../models/steps';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { ISteps, StepsTypes } from "../../models/steps";
 
 @Component({
-  selector: 'app-form-steps',
+  selector: "app-form-steps",
   standalone: true,
   imports: [],
-  templateUrl: './form-steps.component.html',
-  styleUrl: './form-steps.component.css'
+  templateUrl: "./form-steps.component.html",
+  styleUrl: "./form-steps.component.css",
 })
 export class FormStepsComponent {
-  @Input() activeStep : StepsTypes = 0;
-  @Input() steps : ISteps[] = [];
-  @Output() return = new EventEmitter<StepsTypes>(); 
+  @Input() activeStep: StepsTypes = 0;
+  @Input() steps: ISteps[] = [];
+  @Output() return = new EventEmitter<StepsTypes>();
 
-  getStepState(step : ISteps){
-    if(step.type === this.activeStep)
-      return "--active"
-    
-    if(step.completed){
-      return "--completed"  
-    } 
-    return "";
-  } 
+  getStepState(step: ISteps) {
+    if (step.type === this.activeStep) return "--active";
 
-  getSeparatorState(step : ISteps){
-    if(step.completed)
-      return "--completed"
+    if (step.completed) {
+      return "--completed";
+    }
     return "";
   }
 
-  goToStep(step : ISteps){
-    if(step.completed)
+  getSeparatorState(step: ISteps) {
+    if (step.completed) return "--completed";
+    return "";
+  }
+
+  goToStep(step: ISteps) {
+    if (step.completed) {
       this.return.emit(step.type);
+    }
+  }
+
+  isStepClickable(step: ISteps): boolean {
+    return step.completed;
   }
 }
