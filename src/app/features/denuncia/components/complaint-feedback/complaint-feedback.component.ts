c36a2c0754bfca9ba5ebea3f2f67692a275e035b
MathGueff <gueffmatheus@gmail.com>
2025-10-03 19:38:41 -0300

    refactor: corrige estrutura de pastas de features denuncias

---
+++ src/app/features/denuncia/components/complaint-feedback/complaint-feedback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complaint-feedback',
+  standalone: true,
+  imports: [],
+  templateUrl: './complaint-feedback.component.html',
+  styleUrl: './complaint-feedback.component.css'
+})
+export class ComplaintFeedbackComponent {
+
+}
