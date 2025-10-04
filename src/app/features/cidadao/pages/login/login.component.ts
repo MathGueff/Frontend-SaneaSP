e48e5e50477c3791bb97e0d09096cb5ba708d16f
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-10-04 18:16:37 -0300

    feat(login_cidadão): adiciona authService e authTokenService
    adiciona injeção de auth service ao componente de login de cidadão e cria service de local storage para auth token

--- src/app/features/cidadao/pages/login/login.component.ts
+++ src/app/features/cidadao/pages/login/login.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { FormFieldComponent } from "@shared/components/form-field/form-field.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, FormFieldComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css', '../../../../shared/styles/form.style.css']
 })
 export class LoginComponent {
+  loginForm : FormGroup;
 
+  constructor(private fb: NonNullableFormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.min(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 }
