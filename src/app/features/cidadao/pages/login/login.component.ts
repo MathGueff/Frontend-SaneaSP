8dc180515b5f27a40c7a69e5d6c9898865ea386d
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-10-04 20:18:47 -0300

    feat(login): adiciona lógica de login do usuário funcionando

--- src/app/features/cidadao/pages/login/login.component.ts
+++ src/app/features/cidadao/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 import { IUserCredentials } from '@features/usuario/models/user.model';
 import { ToastService } from '@shared/services/toast.service';
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -16,24 +17,28 @@ export class LoginComponent {
 
   private authService = inject(AuthService);
   private toastService = inject(ToastService);
+  private router = inject(Router);
 
   constructor(private fb: NonNullableFormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      senha: ['', [Validators.required, Validators.min(6)]]
+      senha: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
   onSubmit() {
     if(this.loginForm.invalid){
       this.toastService.show({
-        message : "Preencha o formulário corretamente",
+        message : "Informe email e senha para entrar",
         error: true
       })
       return;
     }
     const user : IUserCredentials = this.loginForm.value;
-    this.authService.login(user).subscribe((next) => {
-    });
+    this.authService.login(user).subscribe({
+      next: () => {
+        this.router.navigate(['/cidadao']);
+      },
+    })
   }
 }
