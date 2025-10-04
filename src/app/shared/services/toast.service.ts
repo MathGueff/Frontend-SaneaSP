ac31a89811e52e1a9c954f18962d88f2d988e4ac
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-10-04 19:30:15 -0300

    fix(toast): corrige caminho do ícone errado e z-index

--- src/app/shared/services/toast.service.ts
+++ src/app/shared/services/toast.service.ts
@@ -16,7 +16,7 @@ export class ToastService {
   */
   show(response : IResponse) {
     const image = response.error ? "error.svg" : "success.svg"
-    const iconUrl = `icons/status/color/${image}`;
+    const iconUrl = `icons/status/system/color/${image}`;
 
     const toast : IToast= {
       id : Date.now(), //Para evitar IDs repetidos, utilizando a data de criação 
