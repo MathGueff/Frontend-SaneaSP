5a1526c5e5bf0c069a0c91ef52f879909fa1435b
Ryan <ryanjobs234@gmail.com>
2025-11-07 23:00:44 -0300

    feat: criação do component

---
+++ src/app/core/auth/pages/reset-password-form/reset-password-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password-form',
+  imports: [],
+  templateUrl: './reset-password-form.component.html',
+  styleUrl: './reset-password-form.component.css',
+})
+export class ChangePasswordFormComponent {
+
+}
