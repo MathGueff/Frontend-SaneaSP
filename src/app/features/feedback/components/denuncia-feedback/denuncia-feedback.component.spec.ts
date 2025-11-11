6df383a2289db523608bed6237e74bd1825efcdb
Ryan <ryanjobs234@gmail.com>
2025-11-11 15:01:49 -0300

    chore: component denúncias feedback

---
+++ src/app/features/feedback/components/denuncia-feedback/denuncia-feedback.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DenunciaFeedbackComponent } from './denuncia-feedback.component';
+
+describe('DenunciaFeedbackComponent', () => {
+  let component: DenunciaFeedbackComponent;
+  let fixture: ComponentFixture<DenunciaFeedbackComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DenunciaFeedbackComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DenunciaFeedbackComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/feedback/components/denuncia-feedback/denuncia-feedback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-denuncia-feedback',
+  imports: [],
+  templateUrl: './denuncia-feedback.component.html',
+  styleUrl: './denuncia-feedback.component.css',
+})
+export class DenunciaFeedbackComponent {
+
+}
