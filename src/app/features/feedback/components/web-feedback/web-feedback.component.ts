66b48caf35e798321f7ff1d560975ea9ad5e7fa6
Ryan <ryanjobs234@gmail.com>
2025-11-11 15:02:44 -0300

    chore: feedback do site

---
+++ src/app/features/feedback/components/web-feedback/web-feedback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-feedback',
+  imports: [],
+  templateUrl: './web-feedback.component.html',
+  styleUrl: './web-feedback.component.css',
+})
+export class WebFeedbackComponent {
+
+}
