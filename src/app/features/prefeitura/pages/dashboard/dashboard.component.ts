80aa063b8bbc4648caebd8bb59fbf6d50de7eb4a
MathGueff <gueffmatheus@gmail.com>
2025-11-10 21:21:24 -0300

    feat: adiciona versão inicial do dashboard

---
+++ src/app/features/prefeitura/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ILink } from '@shared/models/link.model';
+
+interface MenuItem {
+  category: string;
+  links: string[];
+}
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+  menu : MenuItem[] = [
+    {
+      category: 'Menu inicial',
+      links: ['Configurando o dashboard', 'Notificações']
+    },
+    {
+      category: 'Denúncias',
+      links: ['Todas denúncias', 'Agendamento de visitas']
+    },
+    {
+      category: 'Relatórios',
+      links: ['Métricas e KPI', 'Análises', 'Exportação']
+    },
+    {
+      category: 'Administrativo',
+      links: ['Gerenciar funcionários', 'Dados da prefeitura']
+    }
+  ]
+}
