0882c74b7b459c0737fb796b36949e8f10b1a237
MathGueff <gueffmatheus@gmail.com>
2025-11-11 21:50:21 -0300

    feat: adiciona routerLink para os links do sidebar

--- src/app/features/prefeitura/pages/dashboard/dashboard.component.ts
+++ src/app/features/prefeitura/pages/dashboard/dashboard.component.ts
@@ -1,34 +1,49 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 import { ILink } from '@shared/models/link.model';
 
 interface MenuItem {
   category: string;
-  links: string[];
+  links: ILink[];
 }
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  menu : MenuItem[] = [
+  menu: MenuItem[] = [
     {
       category: 'Menu inicial',
-      links: ['Configurando o dashboard', 'Notificações']
+      links: [
+        { text: 'Configurando o dashboard', path: '/prefeitura/dashboard/configs' },
+        { text: 'Notificações', path: '/prefeitura/dashboard/notifications' }
+      ]
     },
     {
       category: 'Denúncias',
-      links: ['Todas denúncias', 'Agendamento de visitas', 'Feedbacks dos cidadãos']
+      links: [
+        { text: 'Todas denúncias', path: '/prefeitura/dashboard/all-complaint' },
+        { text: 'Agendamento de visitas', path: '/prefeitura/dashboard/scheduling' },
+        { text: 'Feedbacks dos cidadãos', path: '/prefeitura/dashboard/feedbacks' }
+      ]
     },
     {
       category: 'Relatórios',
-      links: ['Métricas e KPI', 'Análises', 'Exportação']
+      links: [
+        { text: 'Métricas e KPI', path: '/prefeitura/dashboard/kpi' },
+        { text: 'Análises', path: '/prefeitura/dashboard/analysis' },
+        { text: 'Exportação', path: '/prefeitura/dashboard/export' }
+      ]
     },
     {
       category: 'Administrativo',
-      links: ['Gerenciar funcionários', 'Dados da prefeitura']
+      links: [
+        { text: 'Gerenciar funcionários', path: '/prefeitura/dashboard/employees' },
+        { text: 'Dados da prefeitura', path: '/prefeitura/dashboard/me' }
+      ]
     }
   ]
 }
